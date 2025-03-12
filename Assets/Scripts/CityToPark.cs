eb3121709da7d5461c9f14fae6642a02038b23ab
De León <a01800593@tec.mx>
2025-03-11 22:53:33 -0600

    UI

--- Assets/Scripts/CityToPark.cs
+++ Assets/Scripts/CityToPark.cs
@@ -3,13 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class CityToPark : MonoBehaviour
 {
-    public string sceneName = "Park"; // Nombre de la escena de destino
+    public string sceneName = "Park";
+    public float transitionCooldown = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) // Verifica que el Player tenga el tag "Player"
+        if (other.CompareTag("Player") && SceneTransitionManager.instance.canTransition)
         {
-            // Cambiar de escena
+            SceneTransitionManager.instance.StartTransitionCooldown(transitionCooldown);
             SceneManager.LoadScene(sceneName);
         }
     }
--- Assets/Scripts/ParkToCIty.cs
+++ Assets/Scripts/ParkToCIty.cs
@@ -3,12 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class ParkToCity : MonoBehaviour
 {
-    public string sceneName = "City"; // Asegúrate de escribir el nombre EXACTO de la escena a cargar
+    public string sceneName = "City";
+    public float transitionCooldown = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) // El Player debe tener el tag "Player"
+        if (other.CompareTag("Player") && SceneTransitionManager.instance.canTransition)
         {
+            SceneTransitionManager.instance.StartTransitionCooldown(transitionCooldown);
             SceneManager.LoadScene(sceneName);
         }
     }
