93add286fb5b7f666407c13bc5622deadd8fbb98
A01751315 <a01751315@tec.mx>
2025-05-06 09:45:10 -0600

    Direccion IP

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -2,6 +2,6 @@ namespace Variables
 {
     public static class Variables
     {
-        public const string url = "http://54.82.100.45:8080";
+        public const string url = "http://3.95.56.133:8080";
     }
 }
